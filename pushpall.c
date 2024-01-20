2e0681c8a26a7afbb473a040a5b4d13f4f5bb8f1
DYung26 <theadstore1407@gmail.com>
2024-01-20 06:38:44 -0800

    implements the push and pall opcodes.

---
+++ pushpall.c
@@ -0,0 +1,76 @@
+#include "monty.h"
+void push(stack_t **stack, unsigned int value) {
+	stack_t *new_node = malloc(sizeof(stack_t));
+	
+	new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL) {
+        (*stack)->prev = new_node;
+    }
+
+    *stack = new_node;
+}
+
+void pall(stack_t **stack, unsigned int n) {
+	stack_t *current = *stack;
+	unsigned int count = 0;
+
+	while (current != NULL && count < n) {
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+}
+
+void free_stack(stack_t *stack) {
+	stack_t *current = stack;
+	while (current != NULL) {
+		stack_t *next = current->next;
+		free(current);
+		current = next;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	stack_t *stack = NULL;
+	instruction_t instructions[] = {
+		{"push", push},
+    	{"pall", pall},
+    	{NULL, NULL}
+	};
+    int lineNumber = 0;
+	int value;
+
+	char command[10];
+	FILE *file = fopen(argv[1], "r");
+    if (argc != 2) {
+        printf("USAGE: monty %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+    if (file == NULL) {
+        printf("Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+	while (fscanf(file, "%s", command) == 1) {
+		lineNumber++;
+		if (strcmp(command, "push") == 0) {
+			/*int value;*/
+			if (fscanf(file, "%d", &value) == 1) {
+            	instructions[0].f(&stack, value); /*push(stack, value);*/
+			} else {
+				printf("L%d: usage: push integer\n", lineNumber);
+				free_stack(stack);
+    			fclose(file);
+				return EXIT_FAILURE;
+			}
+        } else {
+			instructions[1].f(&stack, value);
+        }
+	}
+	free_stack(stack);
+	fclose(file);
+	return EXIT_SUCCESS;
+}
